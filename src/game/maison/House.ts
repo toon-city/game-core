ec16d94311c4607f0576aff226593a6184019bdf
root <root@PC-MEHDY>
2025-05-14 23:12:10 +0200

    Save

--- src/game/maison/House.ts
+++ src/game/maison/House.ts
@@ -76,8 +76,8 @@ export class House {
           const furniture = new Furniture(
             furnitureData.SID,
             type,
-            parseFloat(furnitureData.PXP) * 2.5 + this.offsetX - 450,
-            parseFloat(furnitureData.PYP) * 2.5 + this.offsetY - 150,
+            parseFloat(furnitureData.PXP) * 2.5 + this.offsetX - 380,
+            parseFloat(furnitureData.PYP) * 2.5 + this.offsetY - 200,
             parseInt(furnitureData.PR),
             0,
             0,
